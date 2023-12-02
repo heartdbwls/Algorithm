b69143924b24672309157f64bb1ba4a6e83cbdc6
심유진 <100303047+heartdbwls@users.noreply.github.com>
2023-12-03 05:48:04 +0900

    Update optimizers.py

--- DNN/optimizers/optimizers.py
+++ DNN/optimizers/optimizers.py
@@ -1,4 +1,12 @@
 import numpy as np
+import cupy as cp
+
+def iterative(sequence, learning_rate=0.01, beta=0):
+	for obj in sequence:
+		if obj.param > 0:
+			obj.weights -= (learning_rate * obj.d_c_w)
+			obj.biases -= (learning_rate * obj.d_c_b)
+
 
 def momentum(sequence, learning_rate=0.1, beta1=0.9, weight_decay=0.0005):  # will have to specify it
 	for obj in sequence:
@@ -7,13 +15,18 @@ def momentum(sequence, learning_rate=0.1, beta1=0.9, weight_decay=0.0005):  # wi
 			obj.weights += obj.w_m
 			obj.b_m = beta1 * obj.b_m - learning_rate * obj.d_c_b - weight_decay * learning_rate * obj.biases
 			obj.biases += obj.b_m
-			
-def adamkern(GRAD, ONE_MINUS_BETA1, ONE_MINUS_BETA2, EPSILON, LEARNING_RATE, PARAM, M, V):
-    M += ONE_MINUS_BETA1 * (GRAD - M)
-    V += ONE_MINUS_BETA2 * (GRAD * GRAD - V)
-    MCAP = M / ONE_MINUS_BETA1
-    VCAP = V / ONE_MINUS_BETA2
-    PARAM -= LEARNING_RATE * (MCAP / (np.sqrt(VCAP) + EPSILON))
+
+
+adamkern = cp.ElementwiseKernel(
+		'T grad, float32 one_minus_beta1, float32 one_minus_beta2, float32 epsilon, float32 learning_rate',
+		'T param, T m, T v',
+		'''	m += one_minus_beta1 * (grad - m);
+			v += one_minus_beta2 * (grad * grad - v);
+			T mcap = m / one_minus_beta1;
+			T vcap = v / one_minus_beta2;
+			param -= learning_rate * (mcap / (sqrt(vcap) + epsilon));''',
+		'adamkern')
+
 
 def adam(sequence, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
 	for obj in sequence:
