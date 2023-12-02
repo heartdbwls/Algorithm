c3db5fdd879d3dccbe9714929e71a00d47f5919a
심유진 <100303047+heartdbwls@users.noreply.github.com>
2023-12-03 04:29:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="dnn_from_scratch",
+    name="dnn",
     version="0.1.dev1",
     author="heartdbwls",
     description="module for DNN",
