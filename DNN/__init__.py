bc85d35967a754869d9547b15562fbf3272f4032
심유진 <100303047+heartdbwls@users.noreply.github.com>
2023-12-03 05:38:36 +0900

    Update __init__.py

--- DNN/__init__.py
+++ DNN/__init__.py
@@ -3,6 +3,5 @@ from . import layers
 from . import optimizers
 from . import autograd
 from . import coled_tracker
-from . import memory_handler
-from . import network
+from . import model
 from . import stream_handler
