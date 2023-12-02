8d5ba88ed83ccdfe7112c75bbf11134ae2c6402a
심유진 <100303047+heartdbwls@users.noreply.github.com>
2023-12-03 05:37:41 +0900

    Update autograd.py

--- DNN/autograd.py
+++ DNN/autograd.py
@@ -65,3 +65,27 @@ class Tensor:
         self.grad = 1
         for v in reversed(topo):
             v._backprop()
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
